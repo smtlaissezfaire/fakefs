758bdd558b28f76dad943e1aeda847c6762bd30a
Scott Taylor <scott@railsnewbie.com>
2009-11-08 16:29:36 -0500

    Move NotImplemented methods into a module

--- lib/fakefs/file.rb
+++ lib/fakefs/file.rb
@@ -1,5 +1,10 @@
+require 'fakefs/not_implemented/file'
+
 module FakeFS
   class File < StringIO
+    include NotImplemented::File::InstanceMethods
+    extend  NotImplemented::File::ClassMethods
+
     PATH_SEPARATOR = '/'
 
     MODES = [
@@ -181,150 +186,6 @@ module FakeFS
       end
     end
 
-    class << self
-      def atime(file_name)
-        raise NotImplementedError
-      end
-
-      def blockdev?(file_name)
-        raise NotImplementedError
-      end
-
-      def chardev?(file_name)
-        raise NotImplementedError
-      end
-
-      def chmod(mode, *files)
-        raise NotImplementedError
-      end
-
-      def chown(mode, *files)
-        raise NotImplementedError
-      end
-
-      def ctime(file_name)
-        raise NotImplementedError
-      end
-
-      def executable?(file_name)
-        raise NotImplementedError
-      end
-
-      def executable_real?(file_name)
-        raise NotImplementedError
-      end
-
-      def fnmatch(pattern, path, flags = nil)
-        raise NotImplementedError
-      end
-
-      alias_method :fnmatch?, :fnmatch
-
-      def for_fd(fd, mode)
-        raise NotImplementedError
-      end
-
-      def foreach(name, sep_string = $/, &block)
-        raise NotImplementedError
-      end
-
-      def ftype(file_name)
-        raise NotImplementedError
-      end
-
-      def grpowned?(file_name)
-        raise NotImplementedError
-      end
-
-      def identical?(file_1, file_2)
-        raise NotImplementedError
-      end
-
-      def lchmod(mode_int, file_name, *args)
-        raise NotImplementedError
-      end
-
-      def lchown(owner_int, group_int, file_name, *args)
-        raise NotImplementedError
-      end
-
-      def lstat
-        raise NotImplementedError
-      end
-
-      def owned?(file_name)
-        raise NotImplementedError
-      end
-
-      def pipe
-        raise NotImplementedError
-      end
-
-      def pipe?(file_name)
-        raise NotImplementedError
-      end
-
-      def popen(cmd_string, mode = READ_ONLY)
-        raise NotImplementedError
-      end
-
-      def readable_real?(file_name)
-        raise NotImplementedError
-      end
-
-      def rename(old_name, new_name)
-        raise NotImplementedError
-      end
-
-      def select(read_array, write_array = nil, error_array = nil, timeout = nil)
-        raise NotImplementedError
-      end
-
-      def setgid?(file_name)
-        raise NotImplementedError
-      end
-
-      def setuid?(set_uid)
-        raise NotImplementedError
-      end
-
-      def socket?(file_name)
-        raise NotImplementedError
-      end
-
-      def split(file_name)
-        raise NotImplementedError
-      end
-
-      def sticky?(file_name)
-        raise NotImplementedError
-      end
-
-      def sysopen
-        raise NotImplementedError
-      end
-
-      def truncate(file_name, integer)
-        raise NotImplementedError
-      end
-
-      def umask(int = nil)
-        raise NotImplementedError
-      end
-
-      def utime(atime, mtime, file_name, *args)
-        raise NotImplementedError
-      end
-
-      def writable_real?(file_name)
-        raise NotImplementedError
-      end
-
-      def zero?(file_name)
-        raise NotImplementedError
-      end
-    end
-
     attr_reader :path
 
     def initialize(path, mode = READ_ONLY, perm = nil)
@@ -372,60 +233,6 @@ module FakeFS
 
     def to_io
       self
-    end
-
-    def write_nonblock(string)
-      raise NotImplementedError
-    end
-
-    def readpartial(maxlen, outbuf = nil)
-      raise NotImplementedError
-    end
-
-    def atime
-      raise NotImplementedError
-    end
-
-    def chmod(mode_int)
-      raise NotImplementedError
-    end
-
-    def chown(owner_int, group_int)
-      raise NotImplementedError
-    end
-
-    def ctime
-      raise NotImplementedError
-    end
-
-    def flock(locking_constant)
-      raise NotImplementedError
-    end
-
-    def lstat
-      raise NotImplementedError
-    end
-
-    def mtime
-      raise NotImplementedError
-    end
-
-    if RUBY_VERSION.to_f >= 1.9
-      def binmode?
-        raise NotImplementedError
-      end
-
-      def close_on_exec=(bool)
-        raise NotImplementedError
-      end
-
-      def close_on_exec?
-        raise NotImplementedError
-      end
-
-      def to_path
-        raise NotImplementedError
-      end
     end
 
   private
